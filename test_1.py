6cceabb01d9c3947c8438c37776d16c597b11bbd
Danila Sukhonosov <gameking90@bk.ru>
2022-05-16 20:55:34 +0400

    тестирую гид

--- test_1.py
+++ test_1.py
@@ -5,4 +5,4 @@ for a in range(1, 151):
           for e in range(1, 151):
             if a ** 5 + b ** 5 + c ** 5 + d ** 5 == e ** 5:
               print(a + b + c + d + e)
-          print()
+          print()1
